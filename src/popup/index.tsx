8898a7da2e632d015a83aa719b25ed97c399a17d
Daisuke <daisuke.kida@nijibox.co.jp>
2024-07-30 16:24:52 +0900

    feat: request frame

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,9 +1,26 @@
-import { useState } from "react"
-
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const handleClick = async () => {
+    const urlParams = new URLSearchParams()
+    urlParams.append("q", "jt")
+    const params = {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+      },
+      method: "POST",
+      body: urlParams
+    }
+    const res = await fetch("https://kabutan.jp/pulldown/", params)
+    const body = await res.json()
+    console.log(body)
+  }
 
-  return <div></div>
+  return (
+    <div>
+      <button type="button" onClick={handleClick}>
+        push
+      </button>
+    </div>
+  )
 }
 
 export default IndexPopup
