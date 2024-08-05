042c8e37355c19ab9c072f9381d2f1c4a35582a9
Daisuke <d.suke.kida@gmail.com>
2024-08-05 23:08:00 +0900

    feat: 入力した文字列に応じて、銘柄候補を検索する機能の実装

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,24 +1,37 @@
+import { useState } from "react"
+
 function IndexPopup() {
-  const handleClick = async () => {
-    const urlParams = new URLSearchParams()
-    urlParams.append("q", "jt")
+  const [labels, setLabels] = useState<string[]>()
+  const urlSearchParam = new URLSearchParams()
+
+  const fetchStockLabel = async (value: string) => {
+    urlSearchParam.append("q", value)
     const params = {
       headers: {
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
       },
       method: "POST",
-      body: urlParams
+      body: urlSearchParam
     }
     const res = await fetch("https://kabutan.jp/pulldown/", params)
-    const body = await res.json()
+    const body: string[] = await res.json()
     console.log(body)
+    return body
+  }
+
+  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    if (!value || value.length < 2) return
+    const fetchedLabels: string[] = await fetchStockLabel(value)
+    setLabels(fetchedLabels)
   }
 
   return (
     <div>
-      <button type="button" onClick={handleClick}>
+      <input type="text" onChange={handleChange} />
+      {/* <button type="button" onClick={handleClick}>
         push
-      </button>
+      </button> */}
     </div>
   )
 }
