08fcf9a12da0a5249a8efb09b3410cee6dc4d811
Daisuke <daisuke.kida@nijibox.co.jp>
2024-08-06 17:26:16 +0900

    feat: 検索機能の実装

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,7 +1,14 @@
 import { useState } from "react"
 
+import "../../style.css"
+
 function IndexPopup() {
-  const [labels, setLabels] = useState<string[]>()
+  const [labelList, setLabelList] = useState<
+    {
+      label: string
+      href: string
+    }[]
+  >()
   const urlSearchParam = new URLSearchParams()
 
   const fetchStockLabel = async (value: string) => {
@@ -21,17 +28,45 @@ function IndexPopup() {
 
   const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value
-    if (!value || value.length < 2) return
+    if (!value || value.length < 2) {
+      setLabelList([])
+      return
+    }
     const fetchedLabels: string[] = await fetchStockLabel(value)
-    setLabels(fetchedLabels)
+    const searchedResults = fetchedLabels.map((fetchedLabel) => {
+      return {
+        label: fetchedLabel,
+        href: `https://kabutan.jp/stock/?code=${fetchedLabel.slice(0, 4)}`
+      }
+    })
+    setLabelList(searchedResults)
   }
 
   return (
-    <div>
-      <input type="text" onChange={handleChange} />
-      {/* <button type="button" onClick={handleClick}>
-        push
-      </button> */}
+    <div className="w-60 py-2 px-4">
+      <input
+        type="text"
+        onChange={handleChange}
+        className="w-full p-1 rounded-md border border-solid border-[#333]"
+      />
+      {labelList ? (
+        <div className="mt-2">
+          <ul className="">
+            {labelList.map((item) => (
+              <li>
+                <a
+                  className="p-1 pl-2 w-full block rounded-md hover:bg-gray-300"
+                  href={item.href}
+                  target="_blank">
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   )
 }
