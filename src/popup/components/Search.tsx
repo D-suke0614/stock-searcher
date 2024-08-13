4f9b0b65ab64f4816de4991779fd083e6a271da1
Daisuke <d.suke.kida@gmail.com>
2024-08-14 00:06:02 +0900

    feat: 検索結果を選択後、詳細情報を表示させる

--- src/popup/components/Search.tsx
+++ src/popup/components/Search.tsx
@@ -1,14 +1,14 @@
-import { useState } from "react"
+import { useState, type MouseEvent } from "react"
 
 const Search = () => {
   const [labelList, setLabelList] = useState<
     {
       label: string
-      href: string
       ticker: string
     }[]
   >()
-  const [hightLightIndex, setHightLightIndex] = useState<number>(0)
+  const [selectedLabel, setSelectedLabel] = useState<string>("")
+  const [selectedStockInfo, setSelectedStockInfo] = useState({})
   const urlSearchParam = new URLSearchParams()
 
   const fetchStockLabel = async (value: string) => {
@@ -27,6 +27,7 @@ const Search = () => {
 
   const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value
+    setSelectedLabel("")
     if (!value || value.length < 2) {
       setLabelList([])
       return
@@ -41,33 +42,87 @@ const Search = () => {
         const ticker = fetchedLabel.slice(0, 4)
         return {
           label: fetchedLabel,
-          href: `https://kabutan.jp/stock/?code=${ticker}`,
           ticker
         }
       })
     setLabelList(searchedResults)
+  }
+
+  const handleLabelClick = async (
+    e: React.MouseEvent<HTMLButtonElement>,
+    ticker: string
+  ) => {
+    const SELECTED_COLOR = "bg-gray-300"
+    if (selectedLabel) {
+      document.getElementById(selectedLabel).classList.remove(SELECTED_COLOR)
+    }
+    e.currentTarget.classList.add(SELECTED_COLOR)
+    setSelectedLabel(ticker)
+    const url = `https://query2.finance.yahoo.com/v8/finance/chart/${ticker}.T?range=1y&interval=1mo&events=div`
+    const res = await fetch(url)
+    const body = await res.json()
+    const result = body.chart.result[0]
+    console.log(result)
+    const dividends = Object.entries(result.events.dividends)
+    const data = {
+      dividends: [dividends[0][1], dividends[1][1]],
+      regularPrice: result.meta.regularMarketPrice
+    }
+    console.log(data)
+    setSelectedStockInfo(data)
   }
   return (
     <>
       <input
         type="text"
         onChange={handleChange}
-        className="w-full p-1 rounded-md border border-solid border-[#333]"
+        className="w-60 p-1 rounded-md border border-solid border-[#333]"
       />
       {labelList ? (
-        <div className="mt-2">
-          <ul>
+        <div className="mt-2 flex">
+          <ul className="w-60">
             {labelList.map((item) => (
               <li key={item.ticker}>
-                <a
-                  className="p-1 pl-2 w-full block rounded-md hover:bg-gray-300"
-                  href={item.href}
-                  target="_blank">
+                <button
+                  id={item.ticker}
+                  className="p-1 pl-2 w-full block text-left rounded-md hover:bg-gray-300"
+                  type="button"
+                  onClick={(e) => handleLabelClick(e, item.ticker)}>
                   {item.label}
-                </a>
+                </button>
               </li>
             ))}
           </ul>
+          {selectedLabel ? (
+            <div className="w-60 ml-3">
+              <dl>
+                <div className="flex">
+                  <dt>株価</dt>
+                  <dd>{selectedStockInfo.regularPrice}</dd>
+                </div>
+                <div className="flex">
+                  {/* 一年前の配当 */}
+                  <dt>配当（2024/03/01）</dt>
+                  <dd>¥{selectedStockInfo.dividends[0].amount}</dd>
+                </div>
+                <div className="flex">
+                  {/* 直近の配当 */}
+                  <dt>配当（2024/09/01）</dt>
+                  <dd>¥{selectedStockInfo.dividends[1].amount}</dd>
+                </div>
+                <div className="flex">
+                  <dt>配当（合計）</dt>
+                  <dd>
+                    ¥
+                    {selectedStockInfo.dividends[0].amount +
+                      selectedStockInfo.dividends[1].amount}
+                  </dd>
+                </div>
+              </dl>
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
       ) : (
         <></>
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -4,7 +4,7 @@ import "../../style.css"
 
 function IndexPopup() {
   return (
-    <div className="w-60 py-2 px-4">
+    <div className="py-2 px-4">
       <Search />
     </div>
   )
